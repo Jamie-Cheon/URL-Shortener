0181effb9089daafbd9bbc5e2623fb6119965961
Jamie-Cheon <jamiecheon55@gmail.com>
2020-06-15 20:11:03 +0900

    initial commit

---
+++ urls/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Url
+
+
+class UrlSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Url
+        fields = ('id', 'origin_url', 'shorten_url', 'created', 'owner')
---
+++ users/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import logout as django_logout
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import viewsets, settings
+from users.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=False)
+    def deactivate(self, request, *args, **kwargs):
+        return super().destroy(self, request, *args, **kwargs)
+
+    @action(detail=False)
+    def logout(self, request):
+        # permission_classes = (AllowAny,)
+        try:
+            request.user.auth_token.delete()
+        except (AttributeError, ObjectDoesNotExist):
+            pass
+        if getattr(settings, 'REST_SESSION_LOGIN', True):
+            django_logout(request)
+
+        response = Response({"detail": "Successfully logged out."},
+                            status=status.HTTP_200_OK)
+        return response
